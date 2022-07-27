60867c61342cb63934a661a3f1c9695ffa0845ff
ignacio russo <ignaciorusso0912@gmail.com>
2022-07-27 18:43:29 -0300

    ruteo

--- app.js
+++ app.js
@@ -5,7 +5,11 @@ var cookieParser = require('cookie-parser');
 var logger = require('morgan');
 
 var indexRouter = require('./routes/index');
-var usersRouter = require('./routes/users');
+var personajesRouter = require("./routes/personajes");
+var galeriaRouter = require("./routes/galeria");
+var temporadasRouter = require("./routes/temporadas");
+var contactoRouter = require("./routes/contacto");
+
 
 var app = express();
 
@@ -20,7 +24,11 @@ app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 
 app.use('/', indexRouter);
-app.use('/users', usersRouter);
+app.use('/personajes', personajesRouter);
+app.use('/galeria', galeriaRouter);
+app.use('/temporadas', temporadasRouter);
+app.use('/contacto', contactoRouter);
+
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
