dc09a01aae1fbb7775d750d83bf66c1e709e44ec
ignacio russo <ignaciorusso0912@gmail.com>
2022-07-27 19:53:32 -0300

    funcionamiento de mail

--- routes/contacto.js
+++ routes/contacto.js
@@ -1,11 +1,47 @@
 var express = require('express');
 var router = express.Router();
+var nodemailer = require("nodemailer");
 
 router.get("/",function(req,res,next){
     res.render("contacto",{
         iscontacto: true
     });
 })
+
+router.post("/",async function(req,res,next){
+    // console.log(req.body)
+
+    var nombre = req.body.nombre;
+    var email = req.body.email;
+    var telefono = req.body.telefono;
+    var comentarios = req.body.comentarios;
+    // console.log(req.body.nombre)
+
+    var obj = {
+        to:"ignaciorusso0912@gmail.com",
+        subject:"contacto desde la pagina web",
+        html: nombre + " se contacto a traves de la web y quiere obtener mas informacion a este correo: " + email + "<br> su telefono es: " + telefono + ". su comentario es: " + comentarios + "."
+    }
+
+    var transport = nodemailer.createTransport({
+        host: process.env.SMTP_HOST,
+        port: process.env.SMTP_PORT,
+        auth:{
+            user: process.env.SMTP_USER,
+            pass: process.env.SMTP_PASS 
+        }
+
+    })
+
+    var informacion = await transport.sendMail(obj);
+
+    res.render("contacto" ,{
+        message: "Mensaje enviado correctamente", iscontacto: true
+    })
+
+})
+
+
 
 
 module.exports = router;
